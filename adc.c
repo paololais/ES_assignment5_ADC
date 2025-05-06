72b35c4500e783ae30430130085e31d80b54e9d8
FilippoSalterini <s4516129@studenti.unige.it>
2025-05-06 11:49:44 +0200

    assignment 3

--- adc.c
+++ adc.c
@@ -29,7 +29,6 @@ void adc_init2() {
     ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
 
     ANSELBbits.ANSB5 = 1;
-    
     TRISBbits.TRISB5 = 1; // Imposta RB5 come input
 
     AD1CON1bits.ADON = 0;       // Turn off ADC to configure
@@ -45,6 +44,33 @@ void adc_init2() {
 
     //AD1CHS0bits.CH0SA = 0;     // Select AN11 as input
     AD1CHS0bits.CH0SA = 5; // Seleziona AN4 (RB4)
+
+    AD1CON1bits.ADON = 1;       // Turn on ADC
+}
+void adc_init3() {
+    // ADC config
+    ANSELA = ANSELB = ANSELC = ANSELD = ANSELE = ANSELG = 0x0000;
+    
+    //Enable AN5 as analog input
+    ANSELBbits.ANSB5 = 1;
+    TRISBbits.TRISB5 = 1; // Imposta RB5 come input
+    
+    //Enable AN11 (RB11) as analog input
+    ANSELBbits.ANSB11 = 1;
+
+    AD1CON1bits.ADON = 0;       // Turn off ADC to configure
+    AD1CON1bits.AD12B = 0;      // 10-bit mode
+    AD1CON1bits.FORM = 0;       // Integer output
+    AD1CON1bits.SSRC = 7;       // Auto conversion after sampling
+    AD1CON1bits.ASAM = 1;       // Manual sampling
+
+    AD1CON2bits.CHPS = 0;       // Use CH0 only
+    AD1CON3bits.ADRC = 0;       // Use system clock
+    AD1CON3bits.SAMC = 16;       // Not used for manual sampling
+    AD1CON3bits.ADCS = 8;       // ADC conversion clock select (Tad)
+
+    AD1CHS0bits.CH0SA = 11;     // Select AN11 as input
+    AD1CHS0bits.CH0SA = 5;      // Seleziona AN4 (RB4)
 
     AD1CON1bits.ADON = 1;       // Turn on ADC
 }
